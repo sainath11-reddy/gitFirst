61f93e973d16154e6151e31a7cac131ef73a72be
sainath11-reddy <sainath11reddy@gmail.com>
2022-09-22 15:41:11 +0530

    second commit

--- dom.js
+++ dom.js
@@ -11,3 +11,8 @@ let itemHead = document.getElementsByClassName("title");
 console.log(itemHead);
 itemHead[0].style.fontWeight = "bold";
 itemHead[0].style.color="green";
+let items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor="green";
+for(let i of items){
+    i.style.fontWeight="bold";
+}
