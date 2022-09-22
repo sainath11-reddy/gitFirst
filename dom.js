ae96ba98c6dfbb07097ff9468e1420d9fc52e6f2
sainath11-reddy <sainath11reddy@gmail.com>
2022-09-22 12:43:16 +0530

    First Commit

---
+++ dom.js
@@ -0,0 +1,13 @@
+let header = document.getElementById("header-title");
+console.log(header);
+// header.innerText="Hello";
+// // header.textContent="Yes"
+// header.innerHTML ="Hello <h2>Yes Please</h2>";
+
+let head = document.getElementById("main-header");
+console.log(head);
+head.style.border="solid 10px black";
+let itemHead = document.getElementsByClassName("title");
+console.log(itemHead);
+itemHead[0].style.fontWeight = "bold";
+itemHead[0].style.color="green";
