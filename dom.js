0b7c7087a2b3cf5b77ffa20286de0530f6e3ecd8
sainath11-reddy <sainath11reddy@gmail.com>
2022-09-22 16:13:12 +0530

    third commit

--- dom.js
+++ dom.js
@@ -4,20 +4,32 @@ console.log(header);
 // // header.textContent="Yes"
 // header.innerHTML ="Hello <h2>Yes Please</h2>";
 
-let head = document.getElementById("main-header");
-console.log(head);
-head.style.border="solid 10px black";
-let itemHead = document.getElementsByClassName("title");
-console.log(itemHead);
-itemHead[0].style.fontWeight = "bold";
-itemHead[0].style.color="green";
-let items = document.getElementsByClassName("list-group-item");
-items[2].style.backgroundColor="green";
-for(let i of items){
-    i.style.fontWeight="bold";
-}
+// let head = document.getElementById("main-header");
+// console.log(head);
+// head.style.border="solid 10px black";
+// let itemHead = document.getElementsByClassName("title");
+// console.log(itemHead);
+// itemHead[0].style.fontWeight = "bold";
+// itemHead[0].style.color="green";
+// let items = document.getElementsByClassName("list-group-item");
+// items[2].style.backgroundColor="green";
+// for(let i of items){
+//     i.style.fontWeight="bold";
+// }
 
-let li = document.getElementsByTagName("li");
-for(let i of li){
-    i.style.color="red";
+// let li = document.getElementsByTagName("li");
+// for(let i of li){
+//     i.style.color="red";
+// }
+
+let secondLi = document.querySelector("li:nth-child(2)");
+secondLi.style.backgroundColor="green";
+let thirdLi = document.querySelector(".list-group-item:nth-child(3)");
+thirdLi.style.color="white";
+
+let items = document.querySelectorAll("li");
+items[1].style.color="green";
+let odd = document.querySelectorAll("li:nth-child(odd)");
+for(let i of odd){
+    i.style.backgroundColor="green";
 }
