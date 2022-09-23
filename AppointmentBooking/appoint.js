16a52f4ec2d9d01fd02714565262f1ca5d92f37f
sainath11-reddy <sainath11reddy@gmail.com>
2022-09-23 17:08:25 +0530

    task12 commit

--- AppointmentBooking/appoint.js
+++ AppointmentBooking/appoint.js
@@ -5,11 +5,30 @@ form.addEventListener("submit", store);
 
 function store(e){
     e.preventDefault();
+    let n1 = document.querySelector("#name").value;
+    let e1 = document.querySelector("#email").value;
     let obj = {
-     name : document.querySelector("#name").value,
-    email : document.querySelector("#email").value
+     name : n1,
+    email :e1
     }
     let objSerialized = JSON.stringify(obj)
-    localStorage.setItem("obj", objSerialized);
-    console.log(localStorage.getItem("obj"));
+    localStorage.setItem("userDetails"+e1+n1, objSerialized);
+    // console.log(localStorage.getItem("obj"));
+    let li = document.createElement("li");
+    li.textContent = obj.name + ' '+obj.email;
+    li.classNme="item";
+    let ul = document.getElementById("users");
+    ul.appendChild(li);
 }
+
+let i=0;
+while(localStorage.key(i)){
+    let v=JSON.parse(localStorage.getItem(localStorage.key(i)));
+    let li = document.createElement("li");
+    li.textContent = v.name + ' '+v.email;
+    li.classNme="item";
+    let ul = document.getElementById("users");
+    ul.appendChild(li);
+    i+=1;
+}
+
