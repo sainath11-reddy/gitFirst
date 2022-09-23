be156a2b7c18393d6de06ee92526c8e7de8473d6
sainath11-reddy <sainath11reddy@gmail.com>
2022-09-23 13:49:57 +0530

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 var form = document.querySelector("#addForm");
 var itemList= document.querySelector("#items");
+var desText = document.createElement("input");
+desText.setAttribute("type","text");
+desText.className="form-control mr-2";
+desText.id="description";
+var sub = document.getElementsByClassName('btn btn-dark')[0];
+// console.log(sub);
+form.insertBefore(desText,sub);
 
 form.addEventListener("submit",addList);
 itemList.addEventListener("click",del)
@@ -8,7 +15,7 @@ function addList(e){
     let te = document.querySelector('#item').value;
     let listTag = document.createElement("li");
     listTag.className="list-group-item";
-    listTag.appendChild(document.createTextNode(te));
+    listTag.appendChild(document.createTextNode(te + " "+ desText.value));
     var delButton = document.createElement('button');
     delButton.className='btn btn-danger btn-sm float-right delete';
     delButton.appendChild(document.createTextNode("X"));
@@ -28,7 +35,7 @@ function del(e){
 
     }
 }
-console.log(itemList)
+// console.log(itemList)
 
 
 for(let i of itemList.children){
@@ -36,5 +43,23 @@ for(let i of itemList.children){
     btn.className="edit-button";
     btn.appendChild(document.createTextNode("edit"));
     i.appendChild(btn);
+}
+
+var filter = document.getElementById("filter");
+
+filter.addEventListener("keyup", filtering);
+
+function filtering(e){
+    let text =e.target.value.toLowerCase();
+    console.log(itemList.children);
+    Array.from(itemList.children).forEach(function(item){
+        var itemName = item.firstChild.textContent.toLowerCase();
+        if(itemName.indexOf(text)!=-1){
+            item.style.display ='block'; 
+        }else{
+            item.style.display='none';
+        }
+    })
+    
 }
 
